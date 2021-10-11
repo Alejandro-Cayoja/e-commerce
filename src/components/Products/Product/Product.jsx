2075c986bf1d64daad9a16bdf19baa15a3fe6c96
Alejandro Cayoja <Stronranger@gmail.com>
2021-10-11 20:02:20 -0300

    importacion de material ui

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconBotton } from '@material-ui/core';
+import { AddShoppingCart } from '@material-ui/icons';
 
 const Product = () => {
     return (
