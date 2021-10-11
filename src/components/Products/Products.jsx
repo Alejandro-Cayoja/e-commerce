6c3f51a67d1f5a6149bacf079e7533a97177c741
Alejandro Cayoja <Stronranger@gmail.com>
2021-10-11 19:23:09 -0300

    producto id nombre y descripcion

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
 
+const products = [
+    { id: 1, name: 'Shoes', description: 'Running shoes.'},
+    { id: 2, name: 'Macbook', description: 'Apple macbook.'},
+];
+
 const Products = () => {
     <main>
         <Grid container justify="center" spacing={4}>
