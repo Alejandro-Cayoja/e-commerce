579c6e7e4a315b18b4b62e0d098363009ea8b715
Alejandro Cayoja <Stronranger@gmail.com>
2021-10-11 20:12:12 -0300

    Arreglo del archivo Productos e implementamos el vinculo con el archivo Producto

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -9,9 +9,9 @@ const products = [
 const Products = () => {
     <main>
         <Grid container justify="center" spacing={4}>
-            {products.map((products) => {
+            {products.map((product) => {
                 <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                    <Product />
+                    <Product product={product} />
                 </Grid>                    
             })}
         </Grid>
