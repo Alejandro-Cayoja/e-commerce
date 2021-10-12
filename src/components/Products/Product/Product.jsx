752d24f12ad1d41eb3d815582d313f680c85c16c
Alejandro Cayoja <Stronranger@gmail.com>
2021-10-11 21:05:16 -0300

    Tipografia diferente para cada dato del producto

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -2,10 +2,23 @@ import React from 'react';
 import { Card, CardMedia, CardContent, CardActions, Typography, IconBotton } from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons';
 
-const Product = () => {
+const Product = ({ product }) => {
     return (
         <Card className={classes.root}>
             <CardMedia className={classes.media} image='' title={product.name} />
+            <CardContent>
+                <div className={classes.cardContent} >
+                    <Typography variant="h5" gutterBottom>
+                        {product.name}
+                    </Typography>
+                    <Typography variant="h5">
+                        {product.price}
+                    </Typography>
+                </div>
+                <Typography variant="h2" color="textSecondary" >
+                    {product.description}
+                </Typography>
+            </CardContent>
         </Card>
     )
 }
